cbe8f20de48714084a595c7e535c63b225f93f33
NkAndre <andre270708@gmail.com>
2026-03-17 20:19:32 -0300

    feat:depencias de navigator instaladas

---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, Image, TextInput, StatusBar, Pressable, Alert } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+export default function Login(){
+const navigation = useNavigation();
+}
