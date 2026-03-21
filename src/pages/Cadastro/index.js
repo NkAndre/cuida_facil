df27d57339bb4ea9f99248a50a3948cddbc3f433
NkAndre <andre270708@gmail.com>
2026-03-21 20:10:43 -0300

    feat: ajuste cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -10,7 +10,7 @@ export default function Cadastro(){
   return(
 
     <View style={styles.container}> 
-        <Text>AIAIIAI</Text>
+        <Text>Hello world</Text>
         <StatusBar style="auto"/>
     </View>
   );
